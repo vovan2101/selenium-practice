4b828077153aff1c5866b528da5886c6b9ecbcbc
Vladimir <veliseykin2000@gmail.com>
2022-11-14 21:46:06 -0800

    .

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -1,18 +1,44 @@
-from selenium import webdriver
+# from selenium import webdriver
 import time
+from seleniumwire import webdriver
+import random
+from fake_useragent import UserAgent
+from proxy_auth_data import login, password
 
 
-url = 'https://www.instagram.com/'
-driver = webdriver.Chrome(executable_path='C:\\Users\\vovae\\Documents\\selenium-practice\\chromedriver\\chromedriver.exe')
+user_agents_list = [
+    'hello_world',
+    'best_of_the_best',
+    'python_today'
+]
+
+useragent =  UserAgent()
+
+options = webdriver.ChromeOptions()
+options.add_argument(f'user-agent={useragent.random}')
+
+
+proxy_options = {
+    'proxy': {
+        'https': f'http://{login}:{password}@138.128.91.65:8000'
+    }
+}
+
+# driver = webdriver.Chrome(
+#     executable_path='C:\\Users\\vovae\\Documents\\selenium-practice\\chromedriver\\chromedriver.exe',
+#      options=options,
+# )
+
+driver = webdriver.Chrome(
+    executable_path='C:\\Users\\vovae\\Documents\\selenium-practice\\chromedriver\\chromedriver.exe',
+     seleniumwire_options=proxy_options
+)
+
 
 try:
-    driver.get(url=url)
+
+    driver.get('https://2ip.ru')
     time.sleep(5)
-    driver.get_screenshot_as_file('1.png')
-    driver.get(url='https://stackoverflow.com/')
-    time.sleep(5)
-    driver.save_screenshot('2.png')
-    time.sleep(2)
 
 except Exception:
     print(Exception)
